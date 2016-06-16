7320eb8172fa5ab96c37b90ab0dcc76409781089
steveAnkit <ankitsharma1702@gmail.com>
2016-06-16 14:57:36 +0530

    Commenting added

--- CodeChef/src/com/practice/SubsequnceSum/SubsequnceSum.java
+++ CodeChef/src/com/practice/SubsequnceSum/SubsequnceSum.java
@@ -10,15 +10,29 @@ public class SubsequnceSum {
 	
 	public static void getSubsequnce(int[] array, int sum){
 		
+		//initialize an array of boolean
 		boolean[] bs = new boolean[256];
 		
+		//make all indexs true which ar present in given array
 		for (int i = 0; i < array.length; i++) {
 			bs[array[i]] = true;
 		}
 		
+		// iterate over array 
 		for (int i = 0; i < array.length; i++) {
+			
+			/*
+			 * apply simple maths like:
+			 * if a + b = sum 
+			 * then
+			 * sum - b = a
+			 */
 			if(bs[(sum - array[i])]){
 				System.out.println("{"+array[i] +","+ (sum - array[i]) +"}");
+				
+				/* For avoiding duplicate sub sets like
+				 * {1,8} and {8,1}
+				 */
 				bs[(array[i])] = false;
 			}
 		}
